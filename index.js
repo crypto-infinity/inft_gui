50171da14465a5f11c7e4d53685f5adcccb89864
Gabriele Scorpaniti <gabriele.scorpaniti@outlook.com>
2023-09-11 12:04:10 +0200

    Added Sample Theme

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const express = require('express'); //Import the express dependency
-const app = express();              //Instantiate an express app, the main work horse of this server
-const port = 5000;                  //Save the port number where your server will be listening
+//dependencies initialization
+const express = require('express');
+const bodyParser = require("body-parser");
 
-//Idiomatic expression in express to route and respond to a client request
+//webserver initialization
+const app = express();              
+const port = 5000;
+
+//webserver listener and registrations
+app.listen(port, () => { console.log(`Now listening on port ${port}`); }); //webserver listener
+
+app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//get & post methods
 app.get('/', (req, res) => {        //get requests to the root ("/") will route here
     res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
-                                                        //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
+                      //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
 });
 
-app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
-    console.log(`Now listening on port ${port}`); 
+app.post("/", (req, res) => {
+    console.log(req.body)
 });
