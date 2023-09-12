b0ebecc09698b3779f87907aa3453f181ab78d81
Gabriele Scorpaniti <gabriele.scorpaniti@outlook.com>
2023-09-12 14:06:45 +0200

    Added registration form - final

--- index.js
+++ index.js
@@ -14,14 +14,12 @@ app.use(express.static(__dirname));
 app.use(bodyParser.urlencoded({ extended: true }))
 
 //get & post methods
-app.get('/', (req, res) => {        //get requests to the root ("/") will route here
-    res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
-                      //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
+app.get('/', (req, res) => {
+    res.render('index');      
 });
 
 app.post("/", (req, res) => {
     console.log(req.body);
-    //res.sendFile('index.html', {root: __dirname}); 
     res.render('index');
 });
 
@@ -29,3 +27,9 @@ app.post("/register", (req, res) => {
     console.log(req.body);
     res.render('register', {name: req.body.name, email: req.body.email, subject: req.body.subject, message: req.body.message});
 });
+
+//TEST DEV REFERENCE
+
+// app.get('/register', (req, res) => {
+//     res.render('register', {name: "notset", email: "notset", subject: "notset", message: "notset"});     
+// });
