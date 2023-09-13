92c7f956ef5ac1b09882101c7ee4081426bbf4fe
Gabriele Scorpaniti <gabriele.scorpaniti@outlook.com>
2023-09-13 18:36:46 +0200

    Updated query

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
 //dependencies initialization
 const express = require('express');
 const bodyParser = require("body-parser");
+const sql = require("mssql");
+var count = 0;
+
+// config for your database
+var config = {
+    server: 'htc-demo.database.windows.net', 
+    database: 'htc-demo',
+    authentication: {
+        type: 'azure-active-directory-default'
+    },
+    trustServerCertificate: false //self-signed cert error
+};
+
+// connect to your database
+sql.connect(config, function (err) {
+    
+    if (err){
+        throw err;
+    } 
+    console.log(`Connected to DB: ${config.server}`);
+    console.log(`ID Count for table: ${count}`);
+
+    // create Request object
+    //var request = new sql.Request();
+       
+    // query to the database and get the records
+    // request.query('select * from Student', function (err, recordset) {
+        
+    //     if (err) console.log(err)
+
+    //     // send records as a response
+    //     res.send(recordset);
+        
+    // });
+});
 
 //webserver initialization
 const app = express();              
@@ -24,12 +59,36 @@ app.post("/", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-    console.log(req.body);
-    res.render('register', {name: req.body.name, email: req.body.email, subject: req.body.subject, message: req.body.message});
+    try
+    {
+        console.log("Querying the DB...")
+        var request = new sql.Request();
+
+        var name = req.body.name;
+        var email = req.body.email;
+        var subject = req.body.subject;
+        var message = req.body.message;
+
+        //var query = 'SELECT * FROM SalesLT.Address'; //DB Query - NON FUNGE
+        var query = `INSERT INTO dbo.test VALUES (${count}, ${name}, ${email}, ${subject}, ${message})`;
+
+        request.query(query, function (err, recordset) { //COME PUBBLICARE QUERY?
+            if (err){
+                console.log("Error: " + err)
+                res.render('./index');
+                throw err;
+            } 
+            count++;
+            // send records as a response, if any
+            console.log(recordset)
+            res.render('register', {name: name, email: email, subject: subject, message: message}); //generate register page with form-passed data
+        });
+    }
+    catch(error){}
 });
 
 //TEST DEV REFERENCE
 
-// app.get('/register', (req, res) => {
-//     res.render('register', {name: "notset", email: "notset", subject: "notset", message: "notset"});     
-// });
+app.get('/register', (req, res) => {
+    res.render('register', {name: "notset", email: "notset", subject: "notset", message: "notset"});     
+});
