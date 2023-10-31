278bf65164ccf7c69c2c68334367a017d8a12f26
Gabriele Scorpaniti <gabriele.scorpaniti@outlook.com>
2023-10-31 18:52:25 +0100

    Fixed library.js

--- js/library.js
+++ js/library.js
@@ -1,14 +1,20 @@
-export function openModal(title, text, scrollTop, scrollLeft) {
+/**
+ * Dialog Methods
+ */
+
+export function openModal(title, text) {
     $('#dialog-title').text(title);
     $('#dialog-text').text(text);
     disableScroll();   
     window.dialog.showModal();
 }
 
+
 export function disableScroll() {
     // Get the current page scroll position
-    scrollTop = window.scrollY || document.documentElement.scrollTop;
-    scrollLeft = window.scrollX || document.documentElement.scrollLeft,
+    var scrollTop = window.scrollY || document.documentElement.scrollTop;
+    
+    var scrollLeft = window.scrollX || document.documentElement.scrollLeft;
 
     // if any scroll is attempted,
     // set this to the previous value
@@ -27,3 +33,33 @@ $('#dialog-close').on('click', function (e) {
     enableScroll();
     dialog.close();
 });
+
+/**
+ * End Dialog Methods
+ */
+
+/**
+ * Image file type check
+ */
+
+// https://developer.mozilla.org/en-US/docs/Web/Media/Formats/Image_types
+const fileTypes = [
+    "image/apng",
+    "image/bmp",
+    "image/gif",
+    "image/jpeg",
+    "image/pjpeg",
+    "image/png",
+    "image/svg+xml",
+    "image/tiff",
+    "image/webp",
+    "image/x-icon",
+];
+
+export function validFileType(file) {
+    return fileTypes.includes(file.type);
+}
+
+/**
+ * End Image file type check
+ */
